6ed00f75f9c79f1a5e572f937a85958f325b8d66
vkcntoread <vkcntoread@gmail.com>
2018-06-06 15:23:39 +0300

    Second line added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello");
+        JOptionPane.showMessageDialog(null, "Helo");
+        JOptionPane.showMessageDialog(null, "Good");
     }
 }
